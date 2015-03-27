3f03f9ea07a824dd9b1f35dd060f495f38cfc88e
Виктор <victor_kzk@mail.ru>
2015-03-27 15:48:28 +0300

    Add controller class. Change UI.

---
+++ Paint/src/application/Controller.java
@@ -0,0 +1,57 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+
+public class Controller {
+	@FXML
+	private Button btnEllipse;
+	@FXML
+	private Button btnCircle;
+	@FXML
+	private Button btnRectangle;
+	@FXML
+	private Button btnSquare;
+	@FXML
+	private Button btnParallelogram;
+	@FXML
+	private Button btnRhombus;
+	@FXML
+	private Canvas canvas;
+	
+	@FXML
+	private void btnEllipseOnClick() {
+		System.out.println("aaa");
+	}
+	
+	@FXML
+	private void btnCircleOnClick() {
+		
+	}
+	
+	@FXML
+	private void btnRectangleOnClick() {
+		
+	}
+	
+	@FXML
+	private void btnSquareOnClick() {
+	
+	}
+	
+	@FXML
+	private void btnParallelogramOnClick() {
+	
+	}
+
+	@FXML
+	private void btnRhombusOnClick() {
+	
+	}
+	
+	@FXML
+	private void canvasOnClick() {
+		
+	}
+}
--- Paint/src/application/MainApp.java
+++ Paint/src/application/MainApp.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import shapes.*;
 
 public class MainApp extends Application {
 	
